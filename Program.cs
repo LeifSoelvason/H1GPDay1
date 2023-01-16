fc29bce9d82beb5ea4664ad3f175856ac360a541
Leif Soelvason <Leif.Soelvason@gmail.com>
2023-01-16 15:18:44 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace H1GPDay1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Converting Celsius to Fahrenheit.
+
+            TemperatureConverter converter1 = new TemperatureConverter();
+            Console.Write("How hot is it in Celcius? ");
+            double celsius = double.Parse(Console.ReadLine());
+            double fahrenheit = converter1.CelsiusToFahrenheit(celsius);
+            Console.WriteLine($"\n{celsius} degree Celsius is equal to {fahrenheit} degree Fahrenheit ");
+            Console.ReadLine();
+
+            Console.Clear();
+
+            //Converting Fahrenheit to Celsius.
+
+            TemperatureConverter converter2 = new TemperatureConverter();
+            Console.Write("How hot is it in Fahrenheit? ");
+            fahrenheit = double.Parse(Console.ReadLine());
+            celsius = converter2.FahrenheitToCelcius(fahrenheit);
+            Console.WriteLine($"\n{fahrenheit} degree Fahrenheit is equal to {celsius} degree Celsius");
+            Console.ReadLine();
+
+            Console.Clear();
+
+        }
+    }
+}
---
+++ TemperatureConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1GPDay1
+{
+    public class TemperatureConverter
+    {
+        //My method on how to convert Celsius to Fahrenheit.
+
+        public double CelsiusToFahrenheit(double celsius)
+        {
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            return fahrenheit;
+        }
+
+        //My method on how to convert Fahrenheit to Celsius.
+
+        public double FahrenheitToCelcius(double fahrenheit)
+        {
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            return celsius;
+        }
+    }
+}
